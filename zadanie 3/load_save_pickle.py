350212ea0bd323d7fe4c24dfede3e4d801b3c9a3
Иван Безуглов <146391288+IvanBzz@users.noreply.github.com>
2023-12-15 04:02:10 +0300

    Delete zadanie 3 directory

--- zadanie 3/load_save_pickle.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def save_table_pickle(file_name, data):
-    with open(file_name, 'wb') as f:
-        pickle.dump(data, f)
-
-def load_table_pickle(file_name):
-    with open(file_name, 'rb') as f:
-        print(pickle.load(f))
